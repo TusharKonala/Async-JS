4d12279be6445e046f0385854a473a8e683d3626
Tushar Konala <tusharkonala284@gmail.com>
2025-11-13 17:14:42 +0530

    Implementing promise

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 console.log("Before");
-getUser(1, userCallback);
+getUser(1, (user) => {
+  getRepositories(user.gitHubUsername, (repos) => {
+    getCommits(repos[0], (commits) => {
+      console.log(commits);
+    });
+  });
+});
 console.log("After");
 
 function getUser(id, callback) {
   setTimeout(() => {
-    console.log("Fetching data from the database...");
-    callback({
-      id: id,
-      gitHubUsername: "Tushar Konala",
-    });
+    console.log("Reading a user from a database...");
+    callback({ id: id, gitHubUsername: "mosh" });
   }, 2000);
-  return 1;
 }
 
 function getRepositories(username, callback) {
   setTimeout(() => {
-    console.log(`Calling GitHub API for finding repo of ${username}..`);
-    callback(["repo-1", "repo-2", "repo-3"]);
+    console.log("Calling GitHub API...");
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
 
-function logRepos(repos) {
-  console.log("Repos: ", repos);
-}
-
-function userCallback(user) {
-  console.log("User", user);
-  getRepositories(user.gitHubUsername, logRepos);
+function getCommits(repo, callback) {
+  setTimeout(() => {
+    console.log("Calling GitHub API...");
+    callback(["commit"]);
+  }, 2000);
 }
