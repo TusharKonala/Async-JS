5950fb8b97c187e1c7c7fdc59d7704575ad4d76d
Tushar Konala <tusharkonala284@gmail.com>
2025-11-13 16:20:03 +0530

    Utilizing callbacks in setTimeout funcs

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 console.log("Before");
-setTimeout(() => {
-  console.log("Fetching data fromt the database...");
-}, 2000);
+getUser(1, (user) => {
+  console.log("User", user);
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log("Repos: ", repos);
+  });
+});
 console.log("After");
+
+function getUser(id, callback) {
+  setTimeout(() => {
+    console.log("Fetching data from the database...");
+    callback({
+      id: id,
+      gitHubUsername: "Tushar Konala",
+    });
+  }, 2000);
+  return 1;
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log(`Calling GitHub API for finding repo of ${username}..`);
+    callback(["repo-1", "repo-2", "repo-3"]);
+  }, 2000);
+}
