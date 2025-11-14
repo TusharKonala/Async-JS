c0070fcd4fd21fd6f9e022b533bf88bbb2e521ff
Tushar Konala <tusharkonala284@gmail.com>
2025-11-14 13:28:59 +0530

    Running parallel promises

--- promise-api.js
+++ promise-api.js
@@ -1,5 +1,24 @@
-const p = Promise.resolve({ id: 1 });
-p.then((result) => console.log(result));
+// const p = Promise.resolve({ id: 1 });
+// p.then((result) => console.log(result));
 
-const q = Promise.reject(new Error("Error message"));
-q.catch((err) => console.log(err.message));
+// const q = Promise.reject(new Error("Error message"));
+// q.catch((err) => console.log(err.message));
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async operation 1...");
+    resolve(1);
+    // reject(new Error("Error message"));
+  }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log("Async operation 2...");
+    resolve(2);
+  }, 2000);
+});
+
+Promise.race([p1, p2])
+  .then((result) => console.log(result))
+  .catch((err) => console.log("Error", err.message));
